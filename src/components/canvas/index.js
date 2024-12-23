6c4d63df689831c30e11fa1c739e27e85f4dbdeb
Pelumi Adejumo <adejumopelumi63@gmail.com>
2024-12-23 13:22:03 +0100

    restart

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import TerminalCanvas from "./Terminal";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Console";
-import StarsCanvas from "./Stars";
-
-export { TerminalCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
